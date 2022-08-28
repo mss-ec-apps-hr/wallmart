af12f55c3bc8ef849564960d1bf607d3badbde3d
hariesh reddy <anandhariytl@gmail.com>
2022-08-28 16:52:46 +0530

    added deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 i am at master branch
 hello guys 
 =======
+=======
+
+this is the line updated now...
+>>>>>>> a341c71 (updated deploy.sh stash)
 i am at master branch 
 hello guys 
 
